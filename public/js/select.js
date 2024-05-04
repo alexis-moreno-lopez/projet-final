e2505439d39e4a545602d9ee9e71676a2001674f
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2024-05-04 15:50:57 +0200

    nav

--- public/js/select.js
+++ public/js/select.js
@@ -6,5 +6,7 @@ function redirectToPage() {
     window.location.href = "/recette";
   } else if (selectedValue === "coach") {
     window.location.href = "/coach";
-  }
+  }else if (selectedValue === "profil") {
+  window.location.href = "/profil";
+}
 }
