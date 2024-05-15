d3d6ea114af073916e03d98f40593722c9becdf1
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2024-05-15 18:33:38 +0200

    css

--- public/js/profil.js
+++ public/js/profil.js
@@ -17,3 +17,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var scrollToTopBtn = document.querySelector('.scroll-to-top');
+  
+    window.addEventListener('scroll', function() {
+      if (window.scrollY > 300) {
+        scrollToTopBtn.style.display = 'block';
+      } else {
+        scrollToTopBtn.style.display = 'none';
+      }
+    });
+  
+    scrollToTopBtn.addEventListener('click', function() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      });
+    });
+  });
