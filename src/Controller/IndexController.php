481fda07d7aa1756b230f659e44d2136746ca598
alexis-moreno-lopez <alexis.morenolopezpro@gmail.com>
2024-05-16 10:07:16 +0200

    install stripe

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 class IndexController extends AbstractController
 {
@@ -15,4 +18,31 @@ class IndexController extends AbstractController
             'controller_name' => 'IndexController',
         ]);
     }
+
+    #[Route('/send_email', name: 'app_send_email')]
+    public function sendEmail(Request $request, MailerInterface $mailer): Response
+    {
+        $email = $request->request->get('email'); // Récupérer l'adresse e-mail à partir du champ de formulaire
+        $prenom = $request->request->get('prenom');
+        $message = $request->request->get('message');
+    
+        if (empty($email)) {
+            // Gérer le cas où l'adresse e-mail est manquante
+            // Par exemple, rediriger l'utilisateur vers la page de formulaire avec un message d'erreur
+            return $this->redirectToRoute('app_index', ['error' => 'Veuillez saisir une adresse e-mail valide.']);
+        }
+    
+        $emailMessage = (new Email())
+            ->from($email) // Utiliser l'adresse e-mail en tant qu'expéditeur
+            ->to('destinataire@example.com')
+            ->subject('Sujet du message')
+            ->text("Prénom : $prenom\nMessage : $message");
+    
+        $mailer->send($emailMessage);
+    
+        return $this->redirectToRoute('app_index');
+}
+
+
+
 }
